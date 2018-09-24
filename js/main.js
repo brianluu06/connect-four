6f4db972d8d36e5c349eb573261b7e949369b7ee
brianluu06 <brianluu06@gmail.com>
2018-09-24 16:54:35 -0700

    Few Changes

--- js/main.js
+++ js/main.js
@@ -1,42 +1,40 @@
 /*----- constants -----*/
 
-
-
+var playerLook;
 
 /*----- app's state (variables) -----*/
 var playerRed;
 var playerYellow;
-
-
-
-
-
-
-
+var board;
+    
 
 /*----- cached element references -----*/
 
 
 
-
-
-
-
-
-
-
 /*----- event listeners -----*/
-
-
-
-
-
-
-
-
-
-
+document.getElementById('board').addEventListener('click', oneClick);
+document.getElementById('1').addEventListener('click', oneClick);
 
 /*----- functions -----*/
+function oneClick(event) {
+    var target = event.target;
+    if (target.tagName !== 'TD') return;
 
+    console.log(event.target);
+};
 
+function initialize (){
+
+    board = [
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,],
+        [null, null, null, null, null, null,]
+    ];
+    winner = null;
+    turn = 1;
+};
