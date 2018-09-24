47020f390b361e4ffb2a34d0433311b296c87b65
brianluu06 <brianluu06@gmail.com>
2018-09-24 14:32:56 -0700

    Updated HTML

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*----- constants -----*/
+
+
+
+
+/*----- app's state (variables) -----*/
+var playerRed;
+var playerYellow;
+
+
+
+
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+
+
+
+
+
+/*----- functions -----*/
+
+
