5ea516f4d2c0934bf91e4567e65aeb77deb3a87f
brianluu06 <brianluu06@gmail.com>
2018-09-25 09:03:09 -0700

    Changes

--- js/main.js
+++ js/main.js
@@ -3,38 +3,74 @@
 var playerLook;
 
 /*----- app's state (variables) -----*/
-var playerRed;
-var playerYellow;
+var playerRed; // x
+var playerYellow; // y
 var board;
-    
+var turn;
 
 /*----- cached element references -----*/
 
 
 
 /*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', oneClick);
-document.getElementById('1').addEventListener('click', oneClick);
+// document.getElementById('board').addEventListener('click', oneClick);
+document.getElementById('button1').addEventListener('click', oneClick);
+document.getElementById('button2').addEventListener('click', oneClick);
+document.getElementById('button3').addEventListener('click', oneClick);
+document.getElementById('button4').addEventListener('click', oneClick);
+document.getElementById('button5').addEventListener('click', oneClick);
+document.getElementById('button6').addEventListener('click', oneClick);
+document.getElementById('button7').addEventListener('click', oneClick);
+
 
 /*----- functions -----*/
 function oneClick(event) {
     var target = event.target;
-    if (target.tagName !== 'TD') return;
+    // if (target.tagName !== 'TD') return;
 
-    console.log(event.target);
-};
+    var columnNum = parseInt(event.target.id[6]) - 1;
+    // console.log(event.target.id[6]); // 1..for loop
+    for (let i = 6; i >= 0; i--) {
+        // const element = board[i][columnNum];
+        if (i === 6) {
+            // console.log(element);
+        }
 
-function initialize (){
+        // if board[i][columnNum] is null 
+        // then set it x or y based on turn
+        // change turn
 
-    board = [
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,],
-        [null, null, null, null, null, null,]
-    ];
-    winner = null;
-    turn = 1;
-};
+        // if (!board[i][columnNum]) {
+        //     if (turn === 1) {
+        //         board[i][columnNum] = 'x'
+        //     }
+            
+        //     if (turn === 2) {
+        //         board[i][columnNum] = 'y'
+        //     }
+
+            
+        // }
+
+        console.log(board);
+
+    };
+
+    function initialize() {
+
+        board = [
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null],
+            [null, null, null, null, null, null]
+        ];
+        winner = null;
+        turn = 1;
+    };
+
+    initialize();
+
+}
